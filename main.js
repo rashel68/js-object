6811e2261183c14ee7fec3e38001109c5b4d5dcf
rashel68 <rashel341068@gmail.com>
2021-08-03 16:10:07 +0600

    js-object notes

---
+++ main.js
@@ -0,0 +1,25 @@
+// Object 
+var laptop = {
+    Brand: 'hp',
+    Generation: '6th Gen',
+    Color: 'Black',
+    Processor: 'Intel Core i3',
+    RAM: '4GB',
+    Storage: '1TB',
+    Screen: '16.5"',
+    Price: 36000
+}
+
+// console.log(laptop);
+// console.log(laptop.Brand, laptop.Price);
+
+
+// Three way to change object properties 
+// laptop.Price = 25000;
+
+// laptop['Price'] = 15000;
+
+// var laptoPrice = 'Price';
+// laptop[laptoPrice] = 20000;
+
+console.log(laptop.Price);
